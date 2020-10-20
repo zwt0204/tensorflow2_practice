3d0c9412454c0c2e838417fcb9f648ea9b2a54f9
weitao.zhang <weitao.zhang@weimob.com>
2020-10-20 17:57:48 +0800

    first

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # tensorflow2_practice
-tf2.0练习
+[查询显卡对应CUDA](https://developer.nvidia.com/cuda-gpus)
+[AMD显卡对GPU的支持](https://medium.com/tensorflow/amd-rocm-gpu-support-for-tensorflow-33c78cc6a6cf)
+[CUDA Toolkit 和 cuDNN 的安装](https://www.tensorflow.org/install/gpu#software_requirements)
+# 参考
+1. [tf2中文文档](https://tf.wiki/zh_hans/basic/installation.html)
---
+++ base/tf基础.py
@@ -0,0 +1,42 @@
+#!/user/bin/env python
+# coding=utf-8
+"""
+@file: tf基础.py
+@author: zwt
+@time: 2020/10/20 16:44
+@desc: 
+"""
+import tensorflow as tf
+
+# 定义一个随机数标量
+random_float = tf.random.uniform(shape=())
+print(random_float)
+
+# 定义一个有两个元素的零向量
+zero_vector = tf.zeros(shape=(2))
+print(zero_vector)
+
+# 定已两个2*2的常量矩阵
+A = tf.constant([[1., 2.], [3., 4.]])
+B = tf.constant([[1., 2.], [3., 4.]])
+
+C = tf.add(A, B)
+print(C)
+print(C.shape)
+print(C.dtype)
+print(C.numpy())
+
+D = tf.matmul(A, B)
+print(D)
+
+# 自动求导机制
+
+x = tf.Variable(initial_value=3.)
+# 在 tf.GradientTape() 的上下文内，所有计算步骤都会被记录以用于求导
+with tf.GradientTape() as tape:
+    y = tf.square(x)
+# 计算y关于x的导数
+y_grad = tape.gradient(y, x)
+print(y, y_grad)
+
+
